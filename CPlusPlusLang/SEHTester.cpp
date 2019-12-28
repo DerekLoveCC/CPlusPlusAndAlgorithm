8dca632e911d631870b04eff04ae3667138b2282
DerekLoveCC <zhaomingbo1010@126.com>
2019-12-28 19:56:36 +0800

    SEH test

--- CPlusPlusLang/SEHTester.cpp
+++ CPlusPlusLang/SEHTester.cpp
@@ -12,18 +12,21 @@ SEHTester::~SEHTester()
 }
 
 void SEHTester::Test() {
-	int i;  cin >> i;
-	try {
-		if (i < 0) throw i;
-		if (i > 0) throw "hello";
-		cout << "i = " << i << endl;
-	}
-	catch (int) {
-		cout << "it is integer" << endl;
-	}
-	catch (const char*) {
-		cout << "it is char array" << endl;
-	}
+    int i;  cin >> i;
+    try {
+        if (i < 0) throw i;
+        if (i > 0) throw "hello";
+        cout << "NO EXCEPTION i = " << i << endl;
+    }
+    catch (long l) {
+        cout << "it is long:" << l << endl;
+    }
+    catch (int e) {
+        cout << "it is integer:" << e << endl;
+    }
+    catch (const char* s) {
+        cout << "it is char array:" << s << endl;
+    }
 
-	cout << "the end of program" << endl;
+    cout << "the end of program" << endl;
 }
